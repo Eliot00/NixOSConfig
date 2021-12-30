4a76ed3e9f4fb83336a66bc1ba4f4924fd1c29ee
Elliot <hack00mind@gmail.com>
2021-12-30 22:47:00 +0800

    feat: git with gpg

--- home.nix
+++ home.nix
@@ -8,6 +8,10 @@
     enable = true;
     userName = "Elliot";
     userEmail = "hack00mind@gmail.com";
+    signing = {
+      signByDefault = true;
+      key = "Elliot (My Github GPG) <hack00mind@gmail.com>";
+    };
     aliases = {
       co = "checkout";
       lg = "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --";
