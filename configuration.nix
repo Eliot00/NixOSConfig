e1e945807f92b0d3d70856db9f3ada317aeaa2d2
Elliot <hack00mind@gmail.com>
2022-01-06 23:12:48 +0800

    chore: enable bluetooth

--- configuration.nix
+++ configuration.nix
@@ -66,6 +66,8 @@
   sound.enable = true;
   hardware.pulseaudio.enable = true;
 
+  hardware.bluetooth.enable = true;
+
   # Enable touchpad support (enabled default in most desktopManager).
   # services.xserver.libinput.enable = true;
 
