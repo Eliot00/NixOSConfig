0de193bd57d7570dd3f5f0966f639d65cc641dc8
Elliot <hack00mind@gmail.com>
2022-02-22 19:53:23 +0800

    feat: enable fcitx5-rime

--- configuration.nix
+++ configuration.nix
@@ -41,8 +41,8 @@
   # };
   i18n.defaultLocale = "zh_CN.UTF-8";
   i18n.inputMethod = {
-    enabled = "ibus";
-    ibus.engines = with pkgs.ibus-engines; [ rime ];
+    enabled = "fcitx5";
+    fcitx5.addons = with pkgs; [ fcitx5-rime fcitx5-chinese-addons ];
   };
 
   # Enable the X11 windowing system.
