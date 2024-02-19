73840e7dcd5219a063f7c37f0915b8c21db5c412
Elliot <hack00mind@gmail.com>
2024-02-19 11:27:55 +0800

    chore: new year update

--- hosts/mac-mini/configuration.nix
+++ hosts/mac-mini/configuration.nix
@@ -4,7 +4,7 @@
     # List packages installed in system profile. To search by name, run:
     # $ nix-env -qaP | grep wget
     environment.systemPackages = with pkgs; [
-        python3Full
+        python3
         pdm
 
         nodejs
