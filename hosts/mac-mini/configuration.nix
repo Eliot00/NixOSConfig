dd649eeb7123474e6721311ef15a64bcac89600e
Elliot <hack00mind@gmail.com>
2024-05-08 10:08:38 +0800

    chore: remove zsh

--- hosts/mac-mini/configuration.nix
+++ hosts/mac-mini/configuration.nix
@@ -68,8 +68,6 @@
         '';
     };
 
-    programs.zsh.enable = true;
-
     # Used for backwards compatibility, please read the changelog before changing.
     # $ darwin-rebuild changelog
     system.stateVersion = 4;
