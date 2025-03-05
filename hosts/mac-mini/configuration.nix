69fc12bbdaa9a3d1d028eed1e8ac78179fb8c295
Elliot <hack00mind@gmail.com>
2025-03-05 14:24:00 +0800

    chore: add ustc mirror

--- hosts/mac-mini/configuration.nix
+++ hosts/mac-mini/configuration.nix
@@ -35,7 +35,10 @@
     nix.settings = {
         experimental-features = "nix-command flakes";
         sandbox = false;
-        substituters = [ "https://mirror.sjtu.edu.cn/nix-channels/store" ];
+        substituters = [
+            "https://mirrors.ustc.edu.cn/nix-channels/store"
+            "https://mirror.sjtu.edu.cn/nix-channels/store"
+        ];
         trusted-users = [ "root" "elliot" ];
     };
 
