3bbfb544620b6b847cc59b8c753f3552b04e6b0d
Elliot <hack00mind@gmail.com>
2025-04-11 10:40:43 +0800

    feat(emacs): apply plus patches

--- modules/emacs/default.nix
+++ modules/emacs/default.nix
@@ -1,12 +1,32 @@
 { pkgs, ... }:
-{
+let
+  my-emacs-mac = pkgs.emacs-unstable-pgtk.overrideAttrs (old: {
+    patches =
+      (old.patches or [])
+      ++ [
+        # Fix OS window role (needed for window managers like yabai)
+        (pkgs.fetchpatch {
+          url = "https://raw.githubusercontent.com/d12frosted/homebrew-emacs-plus/master/patches/emacs-28/fix-window-role.patch";
+          sha256 = "sha256-+z/KfsBm1lvZTZNiMbxzXQGRTjkCFO4QPlEK35upjsE=";
+        })
+        (pkgs.fetchpatch {
+          url = "https://raw.githubusercontent.com/d12frosted/homebrew-emacs-plus/master/patches/emacs-30/round-undecorated-frame.patch";
+          sha256 = "sha256-uYIxNTyfbprx5mCqMNFVrBcLeo+8e21qmBE3lpcnd+4=";
+        })
+        (pkgs.fetchpatch {
+          url = "https://raw.githubusercontent.com/d12frosted/homebrew-emacs-plus/master/patches/emacs-30/system-appearance.patch";
+          sha256 = "sha256-3QLq91AQ6E921/W9nfDjdOUWR8YVsqBAT/W9c1woqAw=";
+        })
+      ];
+  });
+in {
   programs.emacs = {
     enable = true;
     package = pkgs.emacsWithPackagesFromUsePackage {
       config = ./README.org;
       defaultInitFile = true;
       package = if pkgs.system == "aarch64-darwin"
-                   then pkgs.emacs
+                   then my-emacs-mac
                    else pkgs.emacs-pgtk;
       alwaysEnsure = true;
       alwaysTangle = true;
